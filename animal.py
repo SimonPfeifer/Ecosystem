b6a125fa1d8e160b314fc8c2bf491faa5d557e26
Simon Pfeifer <sikon08@hotmail.co.uk>
2018-03-02 19:51:17 +0000

    Added function that wraps coordinates.

--- animal.py
+++ animal.py
@@ -12,12 +12,13 @@ class Animal:
         self.orientation = 0
         self.position = np.random.rand(2) * surface.get_size()
 
-        self.velocity = np.array([0.0, 0.5])#np.random.rand(2) * 10 - 5
+        self.velocity = np.array([0.5, 0], dtype='float')#np.random.rand(2) * 10 - 5
         self.maxvelovity = 1.0
-        self.acceleration = np.array([0, 0, 0])
+        self.acceleration = np.array([0, 0, 0], dtype='float')
         self.maxacceleration = 0.1
 
         # Assign variables used in draw()
+        self.surface = surface
         self.bodydrawsize = 6.0
         self.velocitylinelength = 1.5
         self.innercolour = np.array([random.randint(0,255) for _ in range(3)])
@@ -45,10 +46,10 @@ class Animal:
         #draw.line(surface, self.outercolour, self.position, self.position + self.velocity * self.bodydrawsize, 1)
 
         # Draw whiskers
-        self.whiskersangle = np.linspace(0, 2*np.pi, self.nwhiskers, endpoint=False) + self.orientation
-        self.whiskersendpoint = [self.rotate(self.position, self.position + [0, self.visionrange], angle) for angle in self.whiskersangle]
-        for endpoint in self.whiskersendpoint:
-            draw.line(surface, self.outercolour, self.position, endpoint, 1)
+        # self.whiskersangle = np.linspace(0, 2*np.pi, self.nwhiskers, endpoint=False) + self.orientation
+        # self.whiskersendpoint = [self.rotate(self.position, self.position + [0, self.visionrange], angle) for angle in self.whiskersangle]
+        # for endpoint in self.whiskersendpoint:
+        #     draw.line(surface, self.outercolour, self.position, endpoint, 1)
 
     def whiskers(self, surface):
 
@@ -69,12 +70,29 @@ class Animal:
     def move(self, acceleration=None):
 
         if acceleration == None:
-            self.acceleration = np.zeros(2) #+ [0,0.01]
+            self.acceleration = np.zeros(2) + [0,0.01]
         else:
             self.acceleration = acceleration
         self.velocity += np.clip(self.acceleration, -self.maxacceleration, self.maxacceleration)
         self.position += np.clip(self.velocity, -self.maxvelovity, self.maxvelovity)
+        self.position = self.wrap_coordinates(self.surface, self.position)
         self.acceleration *= 0
+
+    def wrap_coordinates(self, surface, coordinates):
+        width, height = surface.get_size()
+        x, y = coordinates
+
+        if x >= width:
+            x -= width
+        elif x < 0:
+            x += width
+
+        if y >= height:
+            y -= height
+        elif y < 0:
+            y += height 
+
+        return np.array([x, y])
 
     def rotate(self, origin, point, angle):
         ox, oy = origin
